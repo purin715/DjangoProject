c0566dc2235c93ec962ed79d51921cda372a08a3
purin <purin715@naver.com>
2021-07-12 18:10:19 +0900

    7/12 2ban class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create') # as_view() : 해당 뷰를 호출시킨다
 ]
