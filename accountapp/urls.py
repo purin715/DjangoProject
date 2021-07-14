df0bad2642bfcfb97e5475326bbf4834766ccc9c
purin <purin715@naver.com>
2021-07-14 11:55:59 +0900

    7/14 2ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -7,5 +8,9 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
-    path('create/', AccountCreateView.as_view(), name='create') # as_view() : 해당 뷰를 호출시킨다
+    path('login/', LoginView.as_view(template_name = 'accountapp/login.html'), name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('create/', AccountCreateView.as_view(), name='create'), # as_view(): CVD를 사용하기 위해 해당 뷰를 호출시킨다
 ]
