9e0580a141562f1e56b884b76aa79af064a12bba
purin <purin715@naver.com>
2021-07-21 12:40:45 +0900

    7/21 2ban class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
@@ -5,35 +6,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
 
+@login_required()
 def hello_world(request):
     # 라우팅 해줘야 해 (연결)
     # 메인 앱에서 먼저 받고 여기로 보냄
+    if request.method == "POST":
 
-    if request.user.is_authenticated:
-        if request.method == "POST":
+        temp = request.POST.get('input')
 
-            temp = request.POST.get('input')
+        new_data = HelloWorld()
+        new_data.text = temp
+        new_data.save() # 실제 DB에 저장
 
-            new_data = HelloWorld()
-            new_data.text = temp
-            new_data.save() # 실제 DB에 저장
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) # 실제 url 주소를 역치환해준다
 
-            return HttpResponseRedirect(reverse('accountapp:hello_world')) # 실제 url 주소를 역치환해준다
-
-            # data_list = HelloWorld.objects.all() # HelloWorld의 객체들을 가져온다
-            # return render(request, 'accountapp/hello_world.html', context={'data_list' : data_list})
-        else:
-            data_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'data_list' : data_list})
-
+        # data_list = HelloWorld.objects.all() # HelloWorld의 객체들을 가져온다
+        # return render(request, 'accountapp/hello_world.html', context={'data_list' : data_list})
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        data_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'data_list' : data_list})
 
 
 class AccountCreateView(CreateView):
@@ -47,6 +45,9 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user' # html에서 객체를 불러오기 위해
     template_name = 'accountapp/detail.html'
 
+
+@method_decorator(login_required, 'get') # get방식 http에 적용해주겠다
+@method_decorator(login_required, 'post') # post방식 http에 적용해주겠다
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -54,32 +55,12 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user: # pk로 가져온 유저 객체인지 확인
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
--- decorators.py
+++ decorators.py
@@ -10,3 +10,25 @@ def hello_world(input_text):
     print(input_text)
 
 hello_world('Hello World!')
+
+def checker(func):
+    def decorated(width, height):
+        if width > 0 and height > 0:
+            print("True")
+        else:
+            print("False")
+        func(width, height)
+    return decorated
+
+@checker
+def triangle(width, height):
+    area = (width * height) / 2
+    print("삼각형 면적 : ", area)
+
+@checker
+def rectangular(width, height):
+    area = (width * height)
+    print("사각형 면적", area)
+
+triangle(2, 3)
+rectangular(2, 3)
