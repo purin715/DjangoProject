0c96e56f8ce88a2676b8b60eef83edd1641798de
purin <purin715@naver.com>
2021-07-26 17:07:24 +0900

    7/26 2ban class commit 2

--- profileapp/models.py
+++ profileapp/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     # ForeignKeyField가 바라보는 값이 삭제될 때 해당 요소를 처리하는 방법을 지정해 준다.
     # CASCADE : ForeignKeyField를 포함하는 모델 인스턴스(row)도 같이 삭제한다.
 
-    # profile 객체와 연결되어있는 user객체를 거꾸로 불러올때, 이름으로 부르기 위해 related_name 설정
+    # profile 객체와 연결되어있는 user 객체를 거꾸로 불러올때, 이름으로 부르기 위해 related_name 설정
 
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm # 어떤 폼을 입력받아서 할 것인지
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
