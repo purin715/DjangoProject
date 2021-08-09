9bb453d5db94cbfa8d7b4481991bb99d466a87ef
purin <purin715@naver.com>
2021-08-09 17:58:23 +0900

    8/9 2ban class commit 6

--- commentapp/views.py
+++ commentapp/views.py
@@ -13,5 +13,10 @@ class CommentCreateView(CreateView):
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
 
+    def form_valid(self, form):
+        form.instance.writer = self.request.user # 작성자가 누군지 연결
+        form.instance.article_id = self.request.POST.get('article_pk') # 게시글이 뭔지 연결. 템플릿에서 추가적인 정보 넘겨줘야 함
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
