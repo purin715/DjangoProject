1c9c67cd8f513ca4b38fc0870d4d6b02b33eb31a
purin <purin715@naver.com>
2021-08-11 11:15:08 +0900

    8/11 2ban class commit 1

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,19 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView
 
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
 
     def form_valid(self, form):
+        # 정의해주지 않으면, 데이터베이스에 작성자 id와 article id가 null로 반영된다
         # 작성자가 누군지 연결
         form.instance.writer = self.request.user
         # 게시글이 뭔지 연결. 템플릿에서 추가적인 정보 넘겨줘야 함
@@ -25,6 +31,8 @@ class CommentCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
 
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
 class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
